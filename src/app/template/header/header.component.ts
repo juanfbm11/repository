b5411b8488a5ce56d7bcccc1f6faddaa8950b01b
Esteban.Correa <esteban20151901@gmail.com>
2025-05-03 12:04:09 -0500

    Se modifico el Header y el Menu con los botones /   Se añadio el diseño de PRODUCTOS, Se añadieron imagenes

--- src/app/template/header/header.component.ts
+++ src/app/template/header/header.component.ts
@@ -4,7 +4,7 @@ import { Router } from '@angular/router';
 @Component({
   selector: 'app-header',
   standalone:false,  
-  templateUrl: './header.component.html',
+  templateUrl:'./header.component.html',
   styleUrl: './header.component.css',
 })
 export class HeaderComponent {
